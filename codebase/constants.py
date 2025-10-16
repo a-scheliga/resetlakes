038da1ac974ea459ced4e5c75afabd6d4dd579fc
a-scheliga <ann.scheliga@eawag.ch>
2025-10-16 11:46:03 +0200

    added values to handle the newer metadata formats

--- codebase/constants.py
+++ codebase/constants.py
@@ -13,6 +13,7 @@ DEFAULT_START = '1992-09-26'
 DEFAULT_END = '2020-12-31'
 
 VERSION = '2.1.0'
+CCI_DATA_NAME = data_id = 'esacci.LAKES.day.L3S.LK_PRODUCTS.multi-sensor.multi-platform.MERGED.v2-1-0.r1'
 
 PATH_RAW = 'data/raw'
 PATH_EXTRACTED = 'data/extracted'
@@ -23,5 +24,8 @@ PATH_DINEOF = 'data/output/DINEOF'
 PATH_ABBREV = PATH_AUXILIARY+'/abbreviations.json'
 
 FN_MASK = f'lakescci_v{VERSION}_data-availability.shp'
+FN_SHP = f'lakescci_v{VERSION}_data-availability.shp'
 FN_TABLE = f'lakescci_v{VERSION}_metadata.csv'
 URL_TABLE = f'https://climate.esa.int/documents/2607/lakescci_v{VERSION}_metadata.csv'
+
+FN_TABLE_KWARGS = {'delimiter':';'}
